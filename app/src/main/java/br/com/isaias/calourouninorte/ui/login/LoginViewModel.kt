221a03a9178e78b9dd6c7eae5bd644f15052f3a4
Isaias Carvalho <isaiasd.carvalho@gmail.com>
2020-11-12 08:18:58 -0400

    refac: login and register

--- app/src/main/java/br/com/isaias/calourouninorte/ui/login/LoginViewModel.kt
+++ app/src/main/java/br/com/isaias/calourouninorte/ui/login/LoginViewModel.kt
@@ -1,10 +1,12 @@
 package br.com.isaias.calourouninorte.ui.login
 
+import android.util.Patterns
 import androidx.databinding.ObservableField
 import androidx.lifecycle.*
 import br.com.isaias.calourouninorte.R
 import br.com.isaias.calourouninorte.utils.Result
 import br.com.isaias.calourouninorte.data.repository.UserRepository
+import br.com.isaias.calourouninorte.utils.isFilled
 import com.google.android.material.snackbar.Snackbar
 import com.google.firebase.auth.FirebaseUser
 import kotlinx.coroutines.launch
@@ -13,14 +15,35 @@ class LoginViewModel(private val userRepository: UserRepository): ViewModel() {
     private val _userInteraction = MutableLiveData<Boolean>(false)
     val userInteraction: MutableLiveData<Boolean> = _userInteraction
     
-    val username = ObservableField("")
-    val password = ObservableField("")
+    val username = MutableLiveData<String>()
+    val password =  MutableLiveData<String>()
 
     var _createdUser = MutableLiveData<FirebaseUser?>()
 
+    private val _areFieldsFilled = MutableLiveData<Boolean>()
+    val areFieldsFilled: LiveData<Boolean> = _areFieldsFilled
+
+    private val fieldsObserver = Observer<String> { validateSignInFields() }
+
+    init {
+        username.observeForever(fieldsObserver)
+        password.observeForever(fieldsObserver)
+    }
+
+    private fun validateSignInFields() {
+        val isValid = username.value?.isFilled() ?: false && Patterns.EMAIL_ADDRESS.matcher(username.value).matches() && password.value?.isFilled() ?: false
+        _areFieldsFilled.postValue(isValid)
+    }
+
     fun login(): MutableLiveData<FirebaseUser?>{
-        _userInteraction.postValue(false)
-        val response = userRepository.loginInFirebase(username.get() ?: "", password.get() ?: "")
+        val response = userRepository.loginInFirebase(username.value ?: "", password.value ?: "")
         return response
     }
+
+    override fun onCleared() {
+        super.onCleared()
+        username.removeObserver(fieldsObserver)
+        password.removeObserver(fieldsObserver)
+    }
+
 }
--- app/src/main/java/br/com/isaias/calourouninorte/ui/singup/SingUpViewModel.kt
+++ app/src/main/java/br/com/isaias/calourouninorte/ui/singup/SingUpViewModel.kt
@@ -1,23 +1,20 @@
 package br.com.isaias.calourouninorte.ui.singup
 
-import androidx.databinding.ObservableField
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import br.com.isaias.calourouninorte.data.model.User
 import br.com.isaias.calourouninorte.data.repository.UserRepository
-import br.com.isaias.calourouninorte.utils.Result
 import com.google.firebase.auth.FirebaseUser
 import kotlinx.coroutines.launch
 
 class SingUpViewModel(private val repository: UserRepository) : ViewModel() {
 
-    var name = ObservableField("")
-    var email = ObservableField("")
-    var username = ObservableField("")
-    var password = ObservableField("")
-    var confirmPassword = ObservableField("")
+    var name = MutableLiveData<String>()
+    var email = MutableLiveData<String>()
+    var username = MutableLiveData<String>()
+    var password = MutableLiveData<String>()
+    var confirmPassword = MutableLiveData<String>()
 
     private lateinit var user :User
 
@@ -27,10 +24,10 @@ class SingUpViewModel(private val repository: UserRepository) : ViewModel() {
     }
 
     private fun initUser(): User = User(
-        name = name.get() ?: "",
-        email = email.get() ?: "",
-        username = username.get() ?: "",
-        password = password.get() ?: ""
+        name = name.value ?: "",
+        email = email.value ?: "",
+        username = username.value ?: "",
+        password = password.value ?: ""
     )
 
     fun createUser() {
